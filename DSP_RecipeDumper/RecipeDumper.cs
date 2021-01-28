169755a924053ae28b3e19942a7120db4c96d4ed
Matt <matt@theaddisons.us>
2021-01-28 08:08:26 -0500

    tech dumper and toml fixes
    - Adds function to dump Technology table
    - Lowercase booleans (fixes #1)
    - Wrap strings in triple quotes (fixes #1)
    - Wrap recipe type in quotes (fixes #1)
    - Convert item input/output arrays to nested tables. (fixes #1)

--- DSP_RecipeDumper/RecipeDumper.cs
+++ DSP_RecipeDumper/RecipeDumper.cs
@@ -15,6 +15,20 @@ namespace DSP_RecipeDumper
         {
             var harmony = new Harmony("us.evilgeni.dsp_recipedumper"); 
             harmony.PatchAll();
+            //UnityEngine.Debug.Log("[meta]");
+            //UnityEngine.Debug.Log("version = \"\"\"" + GameConfig.gameVersion.ToFullString() + "\"\"\"");
+            //UnityEngine.Debug.Log("build = " + GameConfig.build);
+        }
+
+        [HarmonyPatch(typeof(GlobalObject), "LoadVersions")]
+        public class GlobalObject_LoadVersions
+        {
+            public static void Postfix(GlobalObject __instance)
+            {
+                UnityEngine.Debug.Log("[meta]");
+                UnityEngine.Debug.Log("version = \"\"\"" + GameConfig.gameVersion.ToFullString() + "\"\"\"");
+                UnityEngine.Debug.Log("build = " + GameConfig.build);
+            }
         }
 
         [HarmonyPatch(typeof(RecipeProto), "Preload")]
@@ -23,24 +37,23 @@ namespace DSP_RecipeDumper
             [HarmonyPostfix]
             public static void Postfix(RecipeProto __instance)
             {
-                UnityEngine.Debug.Log("[recipe" + __instance.ID + "]");
-                // UnityEngine.Debug.Log("sid = \"" + __instance.SID + "\"");
-                UnityEngine.Debug.Log("name = \"" + __instance.name + "\"");
+                var name = "recipe" + __instance.ID;
+                UnityEngine.Debug.Log("[" + name + "]");
+                // UnityEngine.Debug.Log("sid = \"\"\"" + __instance.SID + "\"\"\"");
+                UnityEngine.Debug.Log("name = \"\"\"" + __instance.name + "\"\"\"");
                 UnityEngine.Debug.Log("timespend = " + __instance.TimeSpend);
-                UnityEngine.Debug.Log("type = " + __instance.Type);
-                UnityEngine.Debug.Log("handcraft = " + __instance.Handcraft);
-                UnityEngine.Debug.Log("explicit = " + __instance.Explicit);
+                UnityEngine.Debug.Log("type = \"\"\"" + __instance.Type + "\"\"\"");
+                UnityEngine.Debug.Log("handcraft = " + __instance.Handcraft.ToString().ToLower());
+                UnityEngine.Debug.Log("explicit = " + __instance.Explicit.ToString().ToLower());
                 if (__instance.preTech != (TechProto)null)
-                    UnityEngine.Debug.Log("preTech = \"" + __instance.preTech.Name.Translate() + "\"");
-                UnityEngine.Debug.Log("iconPath = \"" + __instance.IconPath + "\"");
-                UnityEngine.Debug.Log("items = [");
+                    UnityEngine.Debug.Log("preTech = \"\"\"" + __instance.preTech.Name.Translate() + "\"\"\"");
+                UnityEngine.Debug.Log("iconPath = \"\"\"" + __instance.IconPath + "\"\"\"");
+                UnityEngine.Debug.Log("[" + name + ".items]");
                 for (int i = 0; i < __instance.Items.Length; i++)
                     UnityEngine.Debug.Log("item" + __instance.Items[i] + " = " + __instance.ItemCounts[i]);
-                UnityEngine.Debug.Log("]");
-                UnityEngine.Debug.Log("results = [");
+                UnityEngine.Debug.Log("[" + name + ".results]");
                 for (int i = 0; i < __instance.Results.Length; i++)
                     UnityEngine.Debug.Log("item" + __instance.Results[i] + " = " + __instance.ResultCounts[i]);
-                UnityEngine.Debug.Log("]");
             }
         }
 
@@ -50,15 +63,65 @@ namespace DSP_RecipeDumper
             [HarmonyPostfix]
             public static void Postfix(ItemProto __instance)
             {
-                UnityEngine.Debug.Log("[item" + __instance.ID + "]");
-                UnityEngine.Debug.Log("name = \"" + __instance.name + "\"");
-                UnityEngine.Debug.Log("miningFrom = \"" + __instance.miningFrom + "\"");
-                UnityEngine.Debug.Log("produceFrom = \"" + __instance.produceFrom + "\"");
-                UnityEngine.Debug.Log("description = \"" + __instance.description + "\"");
+                var name = "item" + __instance.ID;
+                UnityEngine.Debug.Log("[" + name + "]");
+                UnityEngine.Debug.Log("name = \"\"\"" + __instance.name + "\"\"\"");
+                UnityEngine.Debug.Log("miningFrom = \"\"\"" + __instance.miningFrom + "\"\"\"");
+                UnityEngine.Debug.Log("produceFrom = \"\"\"" + __instance.produceFrom + "\"\"\"");
+                UnityEngine.Debug.Log("description = \"\"\"" + __instance.description + "\"\"\"");
                 if (__instance.preTech != (TechProto) null)
-                    UnityEngine.Debug.Log("preTech = \"" + __instance.preTech.Name.Translate() + "\"");
-                UnityEngine.Debug.Log("iconPath = \"" + __instance.IconPath + "\"");
+                    UnityEngine.Debug.Log("preTech = \"\"\"" + __instance.preTech.Name.Translate() + "\"\"\"");
+                UnityEngine.Debug.Log("iconPath = \"\"\"" + __instance.IconPath + "\"\"\"");
+            }
+        }
+
+        [HarmonyPatch(typeof(TechProto), "Preload")]
+        public class TechProto_Preload
+        {
+            [HarmonyPostfix]
+            public static void Postfix(TechProto __instance)
+            {
+                var name = "tech" + __instance.ID;
+                UnityEngine.Debug.Log("[" + name + "]");
+                UnityEngine.Debug.Log("name = \"\"\"" + __instance.name + "\"\"\"");
+                UnityEngine.Debug.Log("description = \"\"\"" + __instance.description + "\"\"\"");
+                UnityEngine.Debug.Log("isLabTech = " + __instance.IsLabTech.ToString().ToLower());
+                UnityEngine.Debug.Log("hashNeeded = " + __instance.HashNeeded);
+                UnityEngine.Debug.Log("iconPath = \"\"\"" + __instance.IconPath + "\"\"\"");
+                UnityEngine.Debug.Log("[" + name + ".preTechs]");
+                for (int i = 0; i < __instance.PreTechs.Length; i++)
+                    UnityEngine.Debug.Log("tech" + __instance.PreTechs[i] + ", ");
+                UnityEngine.Debug.Log("[" + name + ".items]");
+                for (int i = 0; i < __instance.Items.Length; i++)
+                    UnityEngine.Debug.Log("item" + __instance.Items[i] + " = " + __instance.ItemPoints[i]);
+                UnityEngine.Debug.Log("[" + name + ".addItems]");
+                for (int i = 0; i < __instance.AddItems.Length; i++)
+                    UnityEngine.Debug.Log("item" + __instance.AddItems[i] + " = " + __instance.AddItemCounts[i]);
             }
         }
     }
 }
+
+//public static string gameDocumentFolder
+//{
+//    get
+//    {
+//        if (GameConfig.gameDocument == null)
+//        {
+//            GameConfig.gameDocument = new StringBuilder(GameConfig.overrideDocumentFolder).Append(GameConfig.gameName).Append("/").ToString();
+//            if (!Directory.Exists(GameConfig.gameDocument))
+//                Directory.CreateDirectory(GameConfig.gameDocument);
+//        }
+//        return GameConfig.gameDocument;
+//    }
+//}
+
+//string path = GameConfig.gameSaveFolder + saveName + GameSave.saveExt;
+//try
+//{
+//    using (MemoryStream memoryStream = new MemoryStream(4194304))
+//    {
+//        using (BinaryWriter w = new BinaryWriter((Stream)memoryStream))
+//        {
+//using (FileStream fileStream = new FileStream(path, FileMode.Create, FileAccess.Write))
+//    memoryStream.WriteTo((Stream)fileStream);
