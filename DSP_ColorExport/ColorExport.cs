2416ad456f6cc86f2b19253a5486837b3e43cd49
Matt <matt@theaddisons.us>
2021-02-09 20:33:16 -0500

    add colorexport, for, exporting colors

---
+++ DSP_ColorExport/ColorExport.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HarmonyLib;
+using BepInEx;
+
+namespace DSP_ColorExport
+{
+    [BepInPlugin("us.evilgeni.dsp_colorexport", "Color Export Plug-In", "0.0.1.0")]
+    [BepInProcess("DSPGAME.exe")]
+    public class DSP_ColorExportPlugin : BaseUnityPlugin
+    {
+        internal void Awake()
+        {
+            var harmony = new Harmony("us.evilgeni.dsp_colorexport");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(UIVirtualStarmap), "_OnCreate")]
+        public class UIVirtualStarmap__OnCreate
+        {
+            public static void Postfix(UIVirtualStarmap __instance)
+            {
+                UnityEngine.Debug.Log("In UIVirtualStarmap::_OnCreate()");
+                UnityEngine.Debug.Log("neutronStarColor = " + __instance.neutronStarColor);
+                UnityEngine.Debug.Log("whiteDwarfColor = " + __instance.whiteDwarfColor);
+                UnityEngine.Debug.Log("blackholeColor = " + __instance.blackholeColor);
+                for (int i = 0; i < __instance.starColors.colorKeys.Length; i++)
+                    UnityEngine.Debug.Log("colorKey[" + i + "]: " + __instance.starColors.colorKeys[i].color + " @ " + __instance.starColors.colorKeys[i].time);
+                for (int i = 0; i < __instance.starColors.alphaKeys.Length; i++)
+                    UnityEngine.Debug.Log("alphaKey[" + i  + "]: " + __instance.starColors.alphaKeys[i].alpha + " @ " + __instance.starColors.alphaKeys[i].time);
+            }
+        }
+    }
+}
